65b8446055e75d57f3bf923069bf5c4129ae71fa
Roman Yu <roman.yu@tinklabs.com>
2017-02-19 00:14:09 +0800

    Format and add gitignore file.

--- app/src/main/java/com/raywenderlich/cheesefinder/CheeseSearchEngine.java
+++ app/src/main/java/com/raywenderlich/cheesefinder/CheeseSearchEngine.java
@@ -27,32 +27,32 @@ import java.util.List;
 
 public class CheeseSearchEngine {
 
-  private final List<String> mCheeses;
-  private final int mCheesesCount;
+    private final List<String> mCheeses;
+    private final int mCheesesCount;
 
-  public CheeseSearchEngine(List<String> cheeses) {
-    mCheeses = cheeses;
-    mCheesesCount = mCheeses.size();
-  }
-
-  public List<String> search(String query) {
-    query = query.toLowerCase();
-
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    public CheeseSearchEngine(List<String> cheeses) {
+        mCheeses = cheeses;
+        mCheesesCount = mCheeses.size();
     }
 
-    List<String> result = new LinkedList<>();
+    public List<String> search(String query) {
+        query = query.toLowerCase();
 
-    for (int i = 0; i < mCheesesCount; i++) {
-      if (mCheeses.get(i).toLowerCase().contains(query)) {
-        result.add(mCheeses.get(i));
-      }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List<String> result = new LinkedList<>();
+
+        for (int i = 0; i < mCheesesCount; i++) {
+            if (mCheeses.get(i).toLowerCase().contains(query)) {
+                result.add(mCheeses.get(i));
+            }
+        }
+
+        return result;
     }
-
-    return result;
-  }
 
 }
